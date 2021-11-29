5f32f3574b8cd2e09ba3418b7398ebf618ea5911
shiloh92 <84908222+shiloh92@users.noreply.github.com>
2021-11-29 00:48:33 -0800

    bot created

---
+++ bot.js
@@ -0,0 +1,34 @@
+const {
+  Pool
+} = require('pg');
+const dsbot = process.env.DS_BOT;
+
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+client.on('ready', () =>{
+
+console.log("i ready!");
+
+});
+
+
+client.on("messageCreate", async message => {
+
+if(message.content ==="!pint"){
+	message.channel.send("hello, I am not fully operational yet.");
+}
+if(message.content ==="!salad"{
+message.channel.send("hello, I am not fully operational yet.");
+}
+
+
+});
+
+client.login(dsbot);
+ 
+
